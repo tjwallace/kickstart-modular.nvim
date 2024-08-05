e89731ffda8713039ce34f1a50188fceebd54b49
Jeff Wallace <jeff@tjwallace.ca>
2024-08-05 13:07:32 -0700

    Add vim-eunuch

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,6 +12,7 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-eunuch',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
