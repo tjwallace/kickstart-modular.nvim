d8202216942d594f97ca6867e619821403a94263
Jeff Wallace <jeff@tjwallace.ca>
2024-08-05 13:02:07 -0700

    Keymaps for splits

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Splits
+vim.keymap.set('n', '<leader>vs', '<C-w><C-v>', { desc = 'Vertical split' })
+vim.keymap.set('n', '<leader>hs', '<C-w><C-s>', { desc = 'Horizontal split' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
