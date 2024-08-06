f553b5599963dfe63f2a985e4d226f0f6167ee50
Jeff Wallace <jeff.wallace@chime.com>
2024-08-06 13:47:19 -0700

    Add ruby and go to conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -16,17 +16,28 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        local filetype = vim.bo[bufnr].filetype
+
+        -- Disable format_on_save for these filetypes
+        local disable_format_on_save = { ruby = true }
+        if disable_format_on_save[filetype] then
+          return
+        end
+
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_lsp_fallback = { c = true, cpp = true }
+
         return {
           timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          lsp_fallback = not disable_lsp_fallback[filetype],
         }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        ruby = { 'rubocop' },
+        go = { 'goimports', 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
