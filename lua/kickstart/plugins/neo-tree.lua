c03f57a27abdca8b59bd907b86dfab2b6375c24c
Jeff Wallace <jeff.wallace@chime.com>
2024-08-06 09:42:23 -0700

    Don't change cwd when pressing -

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -16,23 +16,20 @@ return {
       '-',
       function()
         local reveal_file = vim.fn.expand '%:p'
-        local dir = vim.fn.getcwd()
         if reveal_file == '' then
           reveal_file = vim.fn.getcwd()
         else
           local f = io.open(reveal_file, 'r')
           if f then
             f.close(f)
-            dir = vim.fn.expand '%:p:h'
           else
             reveal_file = vim.fn.getcwd()
           end
         end
         require('neo-tree.command').execute {
           position = 'current',
-          reveal_file = reveal_file, -- path to file or folder to reveal
-          dir = dir,
-          reveal_force_cwd = true, -- change cwd without asking if needed
+          reveal_file = reveal_file,
+          reveal_force_cwd = true,
         }
       end,
       { des = 'Open neo-tree at current file or working directory' },
@@ -44,7 +41,6 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
-          ['-'] = 'navigate_up',
         },
       },
     },
