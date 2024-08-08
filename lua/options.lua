dadbb8135cfa441945154fc1890d4b9fff3c9aae
Jeff Wallace <jeff.wallace@chime.com>
2024-08-08 08:27:47 -0700

    Set tag width to 2

--- lua/options.lua
+++ lua/options.lua
@@ -62,4 +62,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Set tab width to 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
 -- vim: ts=2 sts=2 sw=2 et
